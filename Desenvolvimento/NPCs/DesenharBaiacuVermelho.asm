0063f3cd0f37594aeaa588d70c6badeefb63d10b
mario <mariolsg.oficial@gmail.com>
2025-02-04 15:30:02 -0300

    .

--- Desenvolvimento/NPCs/DesenharBaiacuVermelho.asm
+++ Desenvolvimento/NPCs/DesenharBaiacuVermelho.asm
@@ -3,9 +3,21 @@
 # RG: $8, $9, $10, $11, $14
 
 desenharbaiacu:
+	
+	sw $31, 0($29)  # Salva o endereço de retorno
+	addi $29, $29, -4  
 
-	sw $31, 0($29)
- 	add $29, $29, -4 
+	# Fazendo o Backup na pilha
+	sw $8, 0($29)    # Salva $8 na pilha
+	addi $29, $29, -4
+	sw $9, 0($29)    # Salva $9 na pilha
+	addi $29, $29, -4
+	sw $10, 0($29)   # Salva $10 na pilha
+	addi $29, $29, -4
+	sw $11, 0($29)   # Salva $11 na pilha
+	addi $29, $29, -4
+	sw $14, 0($29)   # Salva $14 na pilha
+	addi $29, $29, -4
  	
  	# Primeiro endereço de memória para o display
 	lui $8, 0x1001
@@ -84,6 +96,19 @@ desenharbaiacu:
 	
 	ori $9, $0, 0x000000   	# Cor do fantasmaaff   	# Cor do fantasma
 	sw $9,2576($8)
+	
+	# Recuperando o Backup na pilha
+	addi $29, $29, 4
+	lw $14, 0($29)
+	addi $29, $29, 4
+	lw $11, 0($29)
+	addi $29, $29, 4
+	lw $10, 0($29)
+	addi $29, $29, 4
+	lw $9, 0($29)
+	addi $29, $29, 4
+	lw $8, 0($29)
+	addi $29, $29, 4
 	
 retorna:
 	jr $31	

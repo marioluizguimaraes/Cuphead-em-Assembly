142045889c2243501e0fc37a6bceeb925fc6f6a6
mario <mariolsg.oficial@gmail.com>
2025-02-08 19:03:12 -0300

    .

--- Desenvolvimento/NPCs/DesenharBaiacuVerde.asm
+++ Desenvolvimento/NPCs/DesenharBaiacuVerde.asm
@@ -109,6 +109,7 @@ desenharBaiacuVerde:
 	addi $29, $29, 4
 	lw $8, 0($29)
 	addi $29, $29, 4
+	lw $31, 0($29)
 	
 retorna:
 	jr $31	
--- Desenvolvimento/NPCs/subirBaiacu.asm
+++ Desenvolvimento/NPCs/subirBaiacu.asm
@@ -35,6 +35,7 @@ desenharBaiacuVerde:
 	addi $29, $29, 4
 	lw $8, 0($29)
 	addi $29, $29, 4
+	lw $31, 0($29)
 	
 retorna:
 	jr $31	
